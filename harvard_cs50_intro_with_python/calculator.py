6403d7cc0c64756cb09cda73cbc37f7cfc9d13d4
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-16 21:08:03 +0600

    basics of int, float, arithmatic calculations,

---
+++ harvard_cs50_intro_with_python/calculator.py
@@ -0,0 +1,17 @@
+x=float(input("What is x? "))
+y=float(input("What is y? "))
+
+# z=round(x+y, 2) 
+# print(f"{z:,}")    output: 1,000,000
+
+# z=x-y
+# print(z)
+
+# z=x*y
+# print(z)
+
+z=x/y
+print(f"{z:.2f}")
+
+# z=round(x/y, 2)
+# print(z)
