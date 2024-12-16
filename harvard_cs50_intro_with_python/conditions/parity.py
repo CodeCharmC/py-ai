8d5e0898e78c232bac6870f4ebc0e83fe0d1bd69
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-16 21:55:21 +0600

    make parity.py simple

--- harvard_cs50_intro_with_python/conditions/parity.py
+++ harvard_cs50_intro_with_python/conditions/parity.py
@@ -6,9 +6,7 @@ def main():
       print("Ood")
 
 def is_even(n):
-   if n % 2 == 0:
-      return True
-   else:
-      return False
+   # return True if n % 2 == 0 else False    simplified version
+   return n % 2 == 0          #the most simplified version
       
 main()
