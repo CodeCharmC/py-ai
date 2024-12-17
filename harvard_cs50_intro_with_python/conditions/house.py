b4d8610cef62ab40d715a3fe81c5dac6cf7c7d4f
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-17 22:36:20 +0600

    practic if elif else conditins

---
+++ harvard_cs50_intro_with_python/conditions/house.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ").strip().title()
+
+if name == "Harry":
+   print("Gryffindor")
+elif name == "Hermione":
+   print("Gryffindor")
+elif name == "Ron":
+   print("Gryffindor")
+else:
+   print("Who are you?")
