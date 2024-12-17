00375ec6f9613fc4bdbecce42dca9fdf7bad71e0
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-17 23:04:21 +0600

    some best practic

--- harvard_cs50_intro_with_python/loops/cat.py
+++ harvard_cs50_intro_with_python/loops/cat.py
@@ -3,7 +3,12 @@
 # print("meow")
 # print("meow")
 
-n= int(input("How many times should I meow? "))
+# n= int(input("How many times should I meow? "))
+while True:
+   n = int(input("How many times should I meow? "))
+   if n > 0:
+      break
+
 
 # while n > 0:
 #    print("-.-meow", end="")
@@ -12,4 +17,7 @@ n= int(input("How many times should I meow? "))
 # for i in [0, 1, 2, 3]:       a list of numbers = [0, 1, 2, 3]
 #    print("-.-meow", end="")
 for _ in range(n):
-   print("-.-meow", end="")
+   print("-.- meow", end=" ")
+print()
+
+print("@.@ meow\n"*n, end="")
