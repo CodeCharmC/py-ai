2df3262d3c0bc45a66fcc3592eaef84f46d36bbd
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-17 22:42:28 +0600

    practic simplification for if elif else with match case conditions

--- harvard_cs50_intro_with_python/conditions/house.py
+++ harvard_cs50_intro_with_python/conditions/house.py
@@ -1,10 +1,18 @@
 name = input("What is your name? ").strip().title()
 
-if name == "Harry":
-   print("Gryffindor")
-elif name == "Hermione":
-   print("Gryffindor")
-elif name == "Ron":
-   print("Gryffindor")
-else:
-   print("Who are you?")
+# if name == "Harry" or name == "Hermione" or name == "Ron":
+#    print("Gryffindor")
+# elif name == "Draco":
+#    print("Slytherin")
+# else:
+#    print("Who are you?")
+
+# or
+
+match name:
+   case "Harry" | "Hermione" | "Ron":
+      print("Gryffindor")
+   case "Draco":
+      print("Slytherin")
+   case _:
+      print("Who are you?")
