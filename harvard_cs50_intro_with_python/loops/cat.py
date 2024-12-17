7d3f20f030b67f252a88150a1ecc3cafb09718cd
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-17 22:56:57 +0600

    practic loops for-loop

--- harvard_cs50_intro_with_python/loops/cat.py
+++ harvard_cs50_intro_with_python/loops/cat.py
@@ -5,6 +5,11 @@
 
 n= int(input("How many times should I meow? "))
 
-while n > 0:
+# while n > 0:
+#    print("-.-meow", end="")
+#    n -= 1
+
+# for i in [0, 1, 2, 3]:       a list of numbers = [0, 1, 2, 3]
+#    print("-.-meow", end="")
+for _ in range(n):
    print("-.-meow", end="")
-   n -= 1
