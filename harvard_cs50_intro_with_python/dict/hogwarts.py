6e12457d99c1c003fe5b4320943e05ed2201d9d7
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-17 23:35:24 +0600

    list with individual dictionaries

--- harvard_cs50_intro_with_python/dict/hogwarts.py
+++ harvard_cs50_intro_with_python/dict/hogwarts.py
@@ -1,7 +1,8 @@
+#a list
 students_list = ["Harry", "Hermione", "Ron", "Draco", "Neville"]
 houses_list = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin", "Gryffindor"]
 
-
+# a dictionary
 students_dict = {
    "Harry": "Gryffindor", 
    "Hermione": "Gryffindor",
@@ -14,5 +15,18 @@ students_dict = {
 # print(students_dict.get("Harry"))
 # print(students_dict.get("Neville"))
 
-for student in students_dict:
-   print(f"{student} is in {students_dict[student]}")
+# for student in students_dict:
+#    print(f"{student} is in {students_dict[student]}")
+
+
+# a list with individual dictionaries
+students_list_dict = [
+   {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+   {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+   {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+   {"name": "Draco", "house": "Slytherin", "patronus": None},
+   {"name": "Neville", "house": "Gryffindor", "patronus": None}
+]
+
+for student in students_list_dict:
+   print(f"{student['name']} is in {student['house']} and their patronus is {student['patronus']}")
