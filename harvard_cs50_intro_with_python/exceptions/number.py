be4f2f9133501db36158cc7269c5f07d93138356
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-18 17:03:37 +0600

    NameError

--- harvard_cs50_intro_with_python/exceptions/number.py
+++ harvard_cs50_intro_with_python/exceptions/number.py
@@ -4,8 +4,26 @@
 #ValueError: invalid literal for int() with base 10: 'y'  
 
 #solution
+# try:
+#    x = int(input("What's x? "))
+#    print(f"x is {x}")
+# except ValueError:
+#    print("x is not a number")
+
+
 try:
-   x = int(input("What's x? "))
-   print(f"x is {x}")
+   x = int(input("What's x? "))   
 except ValueError:
    print("x is not a number")
+
+print(f"x is {x}")  
+#will give an error of
+#NameError: name 'x' is not defined   or scope error
+
+#solution   
+try:
+   x = int(input("What's x? "))   
+except ValueError:
+   print("x is not a number")
+else:
+   print(f"x is {x}")
