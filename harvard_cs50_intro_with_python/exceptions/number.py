004243474cad0641ed10502b422b936819d15cc1
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-18 19:21:10 +0600

    tying out exceptions

--- harvard_cs50_intro_with_python/exceptions/number.py
+++ harvard_cs50_intro_with_python/exceptions/number.py
@@ -29,11 +29,30 @@
 #    print(f"x is {x}")
 
 # a better solution
-while True:
-   try:
-      x = int(input("What's x? "))  
-      #break    its ok to break here
-   except ValueError:
-      print("x is not a number")
-   else:
-      break
+# while True:
+#    try:
+#       x = int(input("What's x? "))  
+#       #break    its ok to break here
+#    except ValueError:
+#       print("x is not a number")
+#    else:
+#       break
+
+#using function and tying out exceptions
+def main():   
+   x = get_int()
+   print(f"x is {x}")
+
+def get_int():
+   while True:
+      try:
+         # x = int(input("What's x? "))
+         # return x
+         return int(input("What's x? "))         
+      except ValueError:
+         # print("x is not a number")
+         pass
+      # else:
+      #    return x 
+      
+main()
