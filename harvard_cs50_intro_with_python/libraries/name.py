9aecf8167bd5129f00f23f61b8130f9857ea9ef5
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-18 20:37:30 +0600

    good practic

--- harvard_cs50_intro_with_python/libraries/name.py
+++ harvard_cs50_intro_with_python/libraries/name.py
@@ -5,15 +5,15 @@ import sys
 # output could be IndexError: list index out of range
 
 #solution
-try:
-   print("Hello, my name is", sys.argv[1])
-except IndexError:
-   print("Missing name")
+# try:
+#    print("Hello, my name is", sys.argv[1])
+# except IndexError:
+#    print("Missing name")
 
 # another solution
 if len(sys.argv) < 2:
-   print("Too few names")
+   sys.exit("Too few names")
 elif len(sys.argv) > 2:
-   print("Too many names")
-else:
-   print("Hello, my name is", sys.argv[1])
+   sys.exit("Too many names")
+
+print("Hello, my name is", sys.argv[1])
