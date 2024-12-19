1261504fb3afb2f6cf4f94f82f519367ceba51da
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-19 23:42:17 +0600

    convert a csv file to a dictionary

--- harvard_cs50_intro_with_python/file_i_o/list/read_csv/students.py
+++ harvard_cs50_intro_with_python/file_i_o/list/read_csv/students.py
@@ -4,5 +4,5 @@ name = input("What is your name? ").strip().title()
 home = input("Where do you live? ").strip().title()
 
 with open("students.csv", "a") as file:
-   writer = csv.writer(file)
-   writer.writerow([name, home])
+   writer = csv.DictWriter(file, fieldnames=["Name", "Home"])
+   writer.writerow({"Name": name, "Home": home})
