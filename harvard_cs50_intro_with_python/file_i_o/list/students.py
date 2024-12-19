fc60e1499f4f67198d242308d60b550776d9b222
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-19 23:27:32 +0600

    handle csv file using csv module

--- harvard_cs50_intro_with_python/file_i_o/list/students.py
+++ harvard_cs50_intro_with_python/file_i_o/list/students.py
@@ -1,9 +1,10 @@
+import csv
+
 students = []
-with open("students.csv") as file:
-   for line in file:
-      name, house = line.strip().split(",")
-      student = {"name": name, "house": house}
-      students.append(student)
+with open("students.csv", "r") as file:
+   reader = csv.DictReader(file)
+   for name, house in reader:
+      students.append({"name": name, "house": house})
 
 for student in sorted(students, key=lambda student: student["name"]):
    print(f"{student['name']} is from {student[house]}")
