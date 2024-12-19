98a72b09a7b7f2934ce7c3f9c48d4a147a53bd0b
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-19 10:54:27 +0600

    using return because print returns None which will not work in pytest  and test hello.py

--- harvard_cs50_intro_with_python/testing_code/hello.py
+++ harvard_cs50_intro_with_python/testing_code/hello.py
@@ -2,10 +2,10 @@
 def main():
    print("What is your name?")
    name = input().strip().title()
-   hello(name)
+   print(hello(name))
 
-def hello(to="world!"):
-   print("Hello,", to)
+def hello(to="world"):
+   return f"Hello, {to}!"   # using return because print returns None which will not work in pytest
 
 if __name__ == "__main__":
    main()
--- harvard_cs50_intro_with_python/testing_code/test_hello.py
+++ harvard_cs50_intro_with_python/testing_code/test_hello.py
@@ -1,4 +1,7 @@
 from hello import hello
 
-def test_hello():
-   hello("alix") == "Hello, alix"
+def test_hello_default():
+   assert hello() == "Hello, world!"
+
+def test_hello_argument():
+   assert hello("alix") == "Hello, alix!"
