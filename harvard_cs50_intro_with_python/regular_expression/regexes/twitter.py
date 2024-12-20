6c4cbaf84bcb121082cd090f2a95228b87b318f1
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-20 16:45:59 +0600

    collect username from url using re sub

--- harvard_cs50_intro_with_python/regular_expression/regexes/twitter.py
+++ harvard_cs50_intro_with_python/regular_expression/regexes/twitter.py
@@ -2,6 +2,6 @@
 import re
 url = input("URL: ").strip()
 
-username = url.removeprefix("https://twitter.com/", "")
+username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
 print(f"Username: {username}")
 
