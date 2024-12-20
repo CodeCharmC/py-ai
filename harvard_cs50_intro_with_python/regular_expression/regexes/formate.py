7810d8da3f71a4e467bc9c2c34f857c6b96a9f44
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-20 16:12:03 +0600

    create groups to match with the name

--- harvard_cs50_intro_with_python/regular_expression/regexes/formate.py
+++ harvard_cs50_intro_with_python/regular_expression/regexes/formate.py
@@ -1,9 +1,27 @@
 import re 
 name = input("What's your name? ").strip().title()
 
-if " " in name:
-   first, last = name.split(" ")
-   print(f"{last} {first}")
-else:
-   print(name)
+matches = re.search(r"^(.+), (.+)$", name)
+if matches:
+   last, first = matches.groups()
+   name = f"{first} {last}"
+print(f"Hello, {name}")
 
+
+#. means any character
+#^ means start
+#$ means end
+# + means one or more
+# * means zero or more
+# ? means zero or one
+#.+ means one or more characters
+# \d means digit
+# \w means word
+# \s means space
+# \D means not digit
+# \W means not word
+# \S means not space
+# (..) means group
+# | means or
+# \A means start of string
+# \Z means end of string
