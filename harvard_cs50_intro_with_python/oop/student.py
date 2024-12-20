08c818e35d9845b7494893159856b8c974d4ac51
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-20 17:21:00 +0600

    a diffrence between tuple list and dic

--- harvard_cs50_intro_with_python/oop/student.py
+++ harvard_cs50_intro_with_python/oop/student.py
@@ -1,12 +1,13 @@
 def main():
-   student = get_student()
+   student = get_student()   
    print(f"{student[0]} from {student[1]}")
 
 def get_student():
    name = input("Name: ")
    house = input("House: ")
-   return (name, house) #tuple
-   # return {"name": name, "house": house} #dictionary
+   return (name, house) #tuple a tuple is immutable
+   # return [name, house] #list a list is mutable/changeable
+   # return {"name": name, "house": house} #dictionary a dictionary is mutable
 
 
 if __name__ == "__main__":
