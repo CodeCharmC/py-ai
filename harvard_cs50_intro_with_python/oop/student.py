92bc3d5b46e99deca9db0fff3b09af56e2847acd
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-20 20:08:39 +0600

    Difference between tupel_(), dictionary_{}, list_[] and class_[{},{},...]

--- harvard_cs50_intro_with_python/oop/student.py
+++ harvard_cs50_intro_with_python/oop/student.py
@@ -1,14 +1,25 @@
+#difference between tupel_(), dictionary_{}, list_[] and class_[{},{},...]
+
+class Student:
+   ...
+
 def main():
    student = get_student()   
    # print(f"{student[0]} from {student[1]}")#tuple  and list for both
-   print(f"{student['name']} from {student['house']}")#dictionary
+   # print(f"{student['name']} from {student['house']}")#dictionary
+   print(f"{student.name} from {student.house}")#class
 
 def get_student():
-   name = input("Name: ").capitalize()
-   house = input("House: ").capitalize()
+   # name = input("Name: ").capitalize()
+   # house = input("House: ").capitalize()
+   student = Student()
+   student.name = input("Name: ").capitalize()
+   student.house = input("House: ").capitalize()
+
    # return (name, house) #tuple: a tuple is immutable
    # return [name, house] #list: a list is mutable/changeable
-   return {"name": name, "house": house} #dictionary: a dictionary is mutable
+   # return {"name": name, "house": house} #dictionary: a dictionary is mutable   
+   return student #class: a class is mutable
 
 
 if __name__ == "__main__":
