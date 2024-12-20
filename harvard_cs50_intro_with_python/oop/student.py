7d723e8998a77092996a5decad350456e2bd06b8
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-20 17:34:17 +0600

    diffrence between tuple_(), dictionary_{} and list_[]

--- harvard_cs50_intro_with_python/oop/student.py
+++ harvard_cs50_intro_with_python/oop/student.py
@@ -1,13 +1,14 @@
 def main():
    student = get_student()   
-   print(f"{student[0]} from {student[1]}")
+   # print(f"{student[0]} from {student[1]}")#tuple  and list for both
+   print(f"{student['name']} from {student['house']}")#dictionary
 
 def get_student():
-   name = input("Name: ")
-   house = input("House: ")
-   return (name, house) #tuple a tuple is immutable
-   # return [name, house] #list a list is mutable/changeable
-   # return {"name": name, "house": house} #dictionary a dictionary is mutable
+   name = input("Name: ").capitalize()
+   house = input("House: ").capitalize()
+   # return (name, house) #tuple: a tuple is immutable
+   # return [name, house] #list: a list is mutable/changeable
+   return {"name": name, "house": house} #dictionary: a dictionary is mutable
 
 
 if __name__ == "__main__":
