6652c5ea1c726651c371fd0ef28688f9a661510f
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-21 19:42:30 +0600

    some clean up

--- harvard_cs50_intro_with_python/oop/class_methods/student.py
+++ harvard_cs50_intro_with_python/oop/class_methods/student.py
@@ -1,50 +1,34 @@
 class Student:
-   def __init__(self, name, house, patronus=""):      
+   def __init__(self, name, house):      
       self.name = name
       self.house = house
-      # self.patronus = patronus
 
    def __str__(self):
       return f"{self.name} from {self.house}"
    
-   @property            #Getter
+   @property        
    def name(self):
       return self._name
    
-   @name.setter        #Setter
+   @name.setter     
    def name(self, name):
       if not name:
          raise ValueError("Missing name")
       self._name = name
    
-   @property            #Getter
+   @property          
    def house(self):
       return self._house
 
-   @house.setter        #Setter
+   @house.setter       
    def house(self, house): 
       if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
          raise ValueError("Invalid house")
       self._house = house
-      
-
-   # def charm(self):
-   #    match self.patronus:
-   #       case "Stag":
-   #          return "🦌"
-   #       case "Otter":
-   #          return "🦦"
-   #       case "Jack Russell terrier":
-   #          return "🐕"
-   #       case _:
-   #          return "🪄" 
 
 def main():
    student = get_student() 
    print(student)  
-   # print("Expecto Patronum!")
-   # print(student.charm())   
-   # print(f"{student.name} from {student.house}")
 
 def get_student():
    name = input("Name: ").capitalize()
