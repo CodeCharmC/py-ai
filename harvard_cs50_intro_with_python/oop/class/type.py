263ccae955e3049eaf9f88188fec1a43c5e43d5a
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-21 11:33:19 +0600

    python classes type

---
+++ harvard_cs50_intro_with_python/oop/class/type.py
@@ -0,0 +1,32 @@
+print(type(5))
+print(type(5.0))
+print(type("Hello"))
+print(type(True))
+print(type(None))
+print(type([]))
+print(type({}))
+print(type(()))
+print(type(set()))
+print(type(range(0)))
+print(type(print))
+print(type(len))
+print(type(input))
+print(type(dict()))
+
+# these are all classes
+
+#output
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+# <class 'bool'>
+# <class 'NoneType'>
+# <class 'list'>
+# <class 'dict'>
+# <class 'tuple'>
+# <class 'set'>
+# <class 'range'>
+# <class 'builtin_function_or_method'>
+# <class 'builtin_function_or_method'>
+# <class 'builtin_function_or_method'>
+# <class 'dict'>
