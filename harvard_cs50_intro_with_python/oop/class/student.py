39c6ed1b285279c8e8c128ed585595bd7d2a43a1
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-21 11:14:13 +0600

    property getter setter to prevent developer to make changes

--- harvard_cs50_intro_with_python/oop/class/student.py
+++ harvard_cs50_intro_with_python/oop/class/student.py
@@ -2,30 +2,42 @@ class Student:
    def __init__(self, name, house, patronus=""):
       if not name:
          raise ValueError("Missing name")
-      if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-         raise ValueError("Invalid house")
+      
       self.name = name
       self.house = house
-      self.patronus = patronus
+      # self.patronus = patronus
 
    def __str__(self):
       return f"{self.name} from {self.house}"
-   def charm(self):
-      match self.patronus:
-         case "Stag":
-            return "🦌"
-         case "Otter":
-            return "🦦"
-         case "Jack Russell terrier":
-            return "🐕"
-         case _:
-            return "🪄" 
+   
+   @property            #Getter
+   def house(self):
+      return self._house
+
+   @house.setter        #Setter
+   def house(self, house): 
+      if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+         raise ValueError("Invalid house")
+      self._house = house
+      
+
+   # def charm(self):
+   #    match self.patronus:
+   #       case "Stag":
+   #          return "🦌"
+   #       case "Otter":
+   #          return "🦦"
+   #       case "Jack Russell terrier":
+   #          return "🐕"
+   #       case _:
+   #          return "🪄" 
 
 def main():
-   student = get_student()   
-   print("Expecto Patronum!")
-   print(student.charm())
-   # print(student)
+   student = get_student() 
+   # student.house = "Number Four, Privet Drive"
+   print(student)  
+   # print("Expecto Patronum!")
+   # print(student.charm())   
    # print(f"{student.name} from {student.house}")
 
 def get_student():
