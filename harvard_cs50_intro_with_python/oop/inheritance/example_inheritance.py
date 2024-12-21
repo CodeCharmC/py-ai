061c83295948b80e2c330b617dbbf4c4b2cfce77
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-21 20:31:28 +0600

    example of inheritance

---
+++ harvard_cs50_intro_with_python/oop/inheritance/example_inheritance.py
@@ -0,0 +1,22 @@
+#BaseException
+#+--KeyboardInterrupt
+#+--Exception
+#   +--ArithmeticError
+#   |    +--ZeroDivisionError 
+#   +--AssertionError
+#   +--AttributeError
+#   +--EOFError
+#   +--ImportError
+#   |    +--ModuleNotFoundError
+#   +--LookupError
+#   |    +--IndexError
+#   |    +--KeyError
+#   +--NameError
+#   +--SyntaxError
+#   |    +--IndentationError
+#   +--TypeError
+#   +--ValueError
+
+
+#example 1: ValueError inherit some code from Exception(parent) and BaseException(grandparent)
+...
