5cde7806818d1a9c53d2e3f0538e1915e0871bc7
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-22 11:04:06 +0600

    get unique houses from list of dictionaries by using a set()

--- harvard_cs50_intro_with_python/et_cetera/set/houses.py
+++ harvard_cs50_intro_with_python/et_cetera/set/houses.py
@@ -11,12 +11,22 @@ students = [
    {"name": "Lupin", "house": "Slytherin"},
 ]
 
-# get unique houses from this list of dictionaries
+# get unique houses from this list of dictionaries by using a list
 houses = []
 
 for student in students:
    if student["house"] not in houses:
       houses.append(student["house"])
+
+for house in sorted(houses):
+   print(house)
+
+print("-----")
+# get unique houses from this list of dictionaries by using a set() 
+houses = set()
+
+for student in students:
+   houses.add(student["house"])
 
 for house in sorted(houses):
    print(house)
