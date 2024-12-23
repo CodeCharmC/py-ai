11fda3cc97f262302b40a9822d73838cac3187b7
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-23 22:58:05 +0600

    some adjustment

--- harvard_cs50_intro_with_python/say.py
+++ harvard_cs50_intro_with_python/say.py
@@ -5,4 +5,4 @@ engine = pyttsx3.init()
 this = input("What do you want to say? ")
 cowsay.dragon(this)
 engine.say(this)
-engine.runAndWait() #harvard_cs50_intro_with_python\say.py"
+engine.runAndWait() 
