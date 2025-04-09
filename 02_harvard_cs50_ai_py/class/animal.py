20c7436f9a262ebcdc25e8224e5eed1656352206
CodeCharmC <moni.akhtar1066@gmail.com>
2025-04-09 20:27:56 +0600

    practice 🐍 class

--- 02_harvard_cs50_ai_py/class/animal.py
+++ 02_harvard_cs50_ai_py/class/animal.py
@@ -14,4 +14,11 @@ class Dog(Animal):
       print(f"{self.name} barks.")
 
 dog = Dog("Buddy", "Golden Retriever")
-dog.speak()  
+dog1 = Animal("Bob", "German Shepherd")
+dog2 = Dog("Rex", "Labrador")
+dog3 = Dog("Max","Poodle")
+dog4 = Dog("Rover", "Golden Retriever")
+dog5 = Dog("Charlie", "Golden Retriever")
+
+dog.speak() 
+dog1.speak()
