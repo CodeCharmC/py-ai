33bc9e2303d7cc7f6f8cda0d7d963d93b5d6d4d8
CodeCharmC <moni.akhtar1066@gmail.com>
2025-04-16 10:24:10 +0600

    🐍 list

--- 02_harvard_cs50_ai_py/class/list/basic.py
+++ 02_harvard_cs50_ai_py/class/list/basic.py
@@ -1,11 +1,18 @@
 fruits = ["apple", "banana", "cherry"]
 print(fruits[0])  # Output: apple
 print(fruits[2])  # Output: cherry
+
 fruits[1] = "orange"
 print(fruits)  # ['apple', 'orange', 'cherry']
+
 fruits.append("grape")  # adds to the end
 print(fruits)
+
 fruits.remove("apple")  # removes the first match
 print(fruits)
+
 for fruit in fruits:
    print(fruit)
+   
+if "cherry" in fruits:
+   print("Yay! 🍒 is in the list")
