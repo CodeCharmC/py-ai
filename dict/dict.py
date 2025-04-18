98616076456d3c7d43f786632143cef306fe4361
CodeCharmC <moni.akhtar1066@gmail.com>
2025-04-18 12:12:01 +0600

    Access value by key

--- dict/dict.py
+++ dict/dict.py
@@ -2,3 +2,7 @@
 # Creating a simple dictionary
 student = {"name": "Alice", "age": 22, "course": "Computer Science"}
 print(student)
+
+# Access value by key
+print(student["name"])  # Output: Alice
+print(student["age"])   # Output: 22
